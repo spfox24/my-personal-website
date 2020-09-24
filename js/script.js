2cc658aaa54f7621212644de29937b8817875b62
spfox24 <68411909+spfox24@users.noreply.github.com>
2020-09-23 21:52:01 -0500

    adds background image and date function

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// IPO = Input Process Output
+
+// Declare your constants and variables (State)
+let year;
+
+
+// Select and cache DOM element references
+const yearEL = document.getElementById('year');
+
+
+// Initialize your event listeners
+
+
+// Define application functions
+
+// run the function as soon as the page loads
+init();
+
+
+function init() {
+    year = new Date().getFullYear();
+    yearEL.innerText = year;
+}
